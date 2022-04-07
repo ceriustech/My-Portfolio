e21337cd614c31ab704cab5315d39aa55d3abff0
Bill <ceriustech@gmail.com>
2022-04-06 22:10:31 -0500

    updated node modules and added padding to footer

--- client/src/styles/containers/Footer.Styles.js
+++ client/src/styles/containers/Footer.Styles.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-export const FooterContainer = styled.section``;
+export const FooterContainer = styled.section`
+	padding: 100px 15px;
+`;
