3de04a1480327480177d0deb769f934622182f74
Bill <ceriustech@gmail.com>
2022-05-09 07:17:41 -0500

    feat(blog): Restructure markup for blog tile component

--- client/src/components/Blog/BlogTile.Styles.js
+++ client/src/components/Blog/BlogTile.Styles.js
@@ -1,77 +1,25 @@
 import styled from 'styled-components';
 import { defaultColors } from '../../styles/globalstyles/colors.styles';
 import { maxView } from '../../styles/globalstyles/mediaQueries.styles';
+import { Link } from 'react-router-dom';
 import Picture from '../utility/Picture';
 
-export const BlogTileContainer = styled.div`
-	overflow: hidden;
-	padding: 10px;
-`;
+const laptopMax = maxView?.laptop;
+const mobileLargeMax = maxView?.mobileL;
 
-export const BlogTileInner = styled.div`
-	border-radius: 0.25rem;
-	position: relative;
-	-webkit-transition: all 0.4s ease-in-out;
-	transition: all 0.4s ease-in-out;
-	cursor: pointer;
-	width: 320px;
-`;
+const white = defaultColors?.root.whiteColor;
+const black = defaultColors?.root.blackColor;
 
-export const BlogTileImgContainer = styled.div``;
+export const BlogTileContainer = styled.div``;
 
-export const BlogTileDetailsContainer = styled.div`
-	background: #fff;
-	padding: 40px 35px;
-	position: absolute;
-	top: 75%;
-	width: 100%;
-	-webkit-transition: 0.5s ease;
-	transition: 0.5s ease;
+export const BlogTileInner = styled.div``;
 
-	&:hover {
-		height: 100%;
-		-webkit-transform: translate3d(0, -192px, 0);
-		transform: translate3d(0, -192px, 0);
-	}
-`;
+export const BlogTilePicture = styled(Picture)``;
 
-export const BlogTileHeader = styled.h2`
-	color: #52b8ff;
-	text-transform: uppercase;
-	letter-spacing: 1.5px;
-	font-weight: 700;
-	margin-bottom: 1.8em;
-`;
+export const BlogTileOverlay = styled.div``;
 
-export const BlogTileDetail = styled.p`
-	color: #a4a4a4;
-	letter-spacing: 1.5px;
-	font-weight: 600;
-	line-height: 1.5;
-`;
+export const BlogTileHeader = styled.h2``;
 
-export const BlogTileLinkContainer = styled.div`
-	position: absolute;
-	bottom: -15px;
-	z-index: 3;
-	width: 100%;
-	padding-bottom: 15px;
-	background: white;
-`;
+export const BlogCountdownLink = styled(Link)``;
 
-export const BlogLink = styled.a`
-	color: #ff5253;
-	letter-spacing: 1.5px;
-	font-weight: 400;
-	line-height: 1.5;
-	display: inline-block;
-	border-bottom: 2px solid #ff5253;
-	margin-left: 35px;
-	opacity: 0;
-	-webkit-transition: all 0.4s ease-in-out 0.4s;
-	transition: all 0.4s ease-in-out 0.4s;
-
-	&:hover {
-		opacity: 1;
-	}
-`;
+export const BlogLink = styled.a``;
--- client/src/components/Blog/BlogTile.js
+++ client/src/components/Blog/BlogTile.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import {
 	BlogTileContainer,
 	BlogTileInner,
-	BlogTileImgContainer,
-	BlogTileDetailsContainer,
+	BlogTilePicture,
+	BlogTileOverlay,
 	BlogTileHeader,
-	BlogTileDetail,
-	BlogTileLinkContainer,
+	BlogCountdownLink,
 	BlogLink,
 } from './BlogTile.Styles';
 import { SectionWrapper } from '../../styles/globalstyles/container.styles';
-import Picture from '../utility/Picture';
 import { urlFor, client } from '../../client';
 
 const BlogTile = () => {
@@ -25,36 +22,25 @@ const BlogTile = () => {
 		});
 	}, []);
 
+	console.log(blogTile.blogLink);
+
 	return (
 		<BlogTileContainer>
 			<SectionWrapper flexDirection="row">
-				{blogTile.map((tile, index) => (
-					<BlogTileInner key={index}>
-						<BlogTileImgContainer>
-							<Picture
-								className="img"
-								image={urlFor(tile.imgUrl)}
-								srcset={urlFor(tile.imgUrl)}
-								altText={tile.altText}
-							/>
-						</BlogTileImgContainer>
-						<BlogTileDetailsContainer>
-							<BlogTileHeader>{tile.title}</BlogTileHeader>
-							<BlogTileDetail>{tile.description}</BlogTileDetail>
-						</BlogTileDetailsContainer>
-						<BlogTileLinkContainer>
-							{tile.blogLink === undefined ? (
-								<Link to="/countdown/:lifengineered" rel="noreferrer">
-									Read More
-								</Link>
-							) : (
-								<BlogLink href="PageCountdown/countdown.html">
-									Read More
-								</BlogLink>
-							)}
-						</BlogTileLinkContainer>
-					</BlogTileInner>
-				))}
+				<BlogTileInner class="hovereffect panel image-1 .hovereffect .overlay">
+					<BlogTilePicture
+						class="center portfolio_img "
+						src="Img/LimbleMedia.png"
+						alt="flat-design"
+					/>
+					<BlogTileOverlay class="overlay">
+						<BlogTileHeader class="portfolio_h2">LimbleMedia</BlogTileHeader>
+
+						<BlogCountdownLink to="/countdown">
+							<p class="portfolio-link icon-links">Take a look</p>
+						</BlogCountdownLink>
+					</BlogTileOverlay>
+				</BlogTileInner>
 			</SectionWrapper>
 		</BlogTileContainer>
 	);
