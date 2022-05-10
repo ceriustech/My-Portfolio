34f83d8f9df49c01a071d5ed8ddbb013e53a6a68
Bill <ceriustech@gmail.com>
2022-05-10 11:51:00 -0500

    feat(blog schema): Added an author field to blog schema

--- backend/schemas/blog.js
+++ backend/schemas/blog.js
@@ -9,6 +9,11 @@ export default {
 			type: 'string',
 		},
 		{
+			name: 'author',
+			title: 'Author',
+			type: 'string',
+		},
+		{
 			name: 'description',
 			title: 'Description',
 			type: 'string',
