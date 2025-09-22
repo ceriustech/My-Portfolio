c21a9f054fd6a9cb1aff58786af08d8e793cb0b8
Bilal Masters <ceriustech@gmail.com>
2025-09-22 01:39:59 -0500

    feat: add link to app showcase cards

--- src/page_sections/portfolio_section/components/project_card/index.jsx
+++ src/page_sections/portfolio_section/components/project_card/index.jsx
@@ -7,15 +7,29 @@ const ProjectCard = forwardRef(({ project }, ref) => {
 		image,
 		alt,
 		className: projectClassName,
+		url,
 	} = project;
+
+	const URL = url ? url : '';
+	const urlTarget = url.startsWith('http') ? '_blank' : undefined;
+	const noOpenRefer = url.startsWith('http')
+		? 'noopener noreferrer'
+		: undefined;
 
 	return (
 		<div ref={ref} className={`${projectClassName} group`}>
 			<img src={image} alt={alt} />
 
-			<div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+			<div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-75 transition-opacity duration-300">
 				<div className="absolute bottom-0 left-0 right-0 p-4">
-					<h3>{title}</h3>
+					<a
+						className="cursor-pointer"
+						href={url}
+						target={urlTarget}
+						rel={noOpenRefer}
+					>
+						<h3>{title}</h3>
+					</a>
 					{description && <p>{description}</p>}
 				</div>
 			</div>
--- src/page_sections/portfolio_section/constants.js
+++ src/page_sections/portfolio_section/constants.js
@@ -1,84 +1,35 @@
 // src/page_sections/portfolio_section/constants.js
 export const PROJECT_CARDS = [
 	{
-		id: 'ryde',
+		id: 'ChatExt',
 		title: 'ChatExt',
 		description:
 			'A chrome extension that enables files to be uploaded into ChatGPT',
 		image: '/images/chatext-cover.png',
-		alt: 'Ryde App Interface',
+		alt: 'chatgpt chrome extension',
 		backgroundColor: null, // Featured projects might not need background colors
 		className: 'featured-project-wrapper',
+		url: 'https://chatext.app/',
 	},
 	{
-		id: 'library',
-		title: 'Library Management Platform with Advanced Search',
+		id: 'qrgen',
+		title: 'QrGen',
+		description: 'A website that allows users to generate qr codes',
 		image: '/images/qrgen.png',
-		alt: 'Library Management Platform',
+		alt: 'qrcode generator',
 		backgroundColor: '#FFEFDB',
 		className: 'project-list-wrapper',
+		url: 'https://qrgen.app/',
 	},
 	{
-		id: 'yc-directory',
-		title: 'YC Directory App for Startup Discovery',
-		image: '/images/project3.png',
+		id: 'apply-wiser',
+		title: 'ApplyWiser',
+		description:
+			'A platform that uses AI to auto modify resumes to fit job descriptions',
+		image: '/images/apply-wiser-logo.png',
 		alt: 'YC Directory App',
 		backgroundColor: '#FFE7EB',
 		className: 'project-list-wrapper',
+		url: '',
 	},
-	{
-		id: 'yc-directory',
-		title: 'YC Directory App for Startup Discovery',
-		image:
-			'https://images.unsplash.com/photo-1493770348161-369560ae357d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxmb29kfGVufDB8MHx8fDE3MjEwNDI2MTR8MA&ixlib=rb-4.0.3&q=80&w=1080',
-		alt: 'YC Directory App',
-		backgroundColor: '#FFE7EB',
-		className: 'project-list-wrapper',
-	},
-	{
-		id: 'yc-directory',
-		title: 'YC Directory App for Startup Discovery',
-		image:
-			'https://images.unsplash.com/photo-1493770348161-369560ae357d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxmb29kfGVufDB8MHx8fDE3MjEwNDI2MTR8MA&ixlib=rb-4.0.3&q=80&w=1080',
-		alt: 'YC Directory App',
-		backgroundColor: '#FFE7EB',
-		className: 'project-list-wrapper',
-	},
-	{
-		id: 'yc-directory',
-		title: 'YC Directory App for Startup Discovery',
-		image:
-			'https://images.unsplash.com/photo-1493770348161-369560ae357d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxmb29kfGVufDB8MHx8fDE3MjEwNDI2MTR8MA&ixlib=rb-4.0.3&q=80&w=1080',
-		alt: 'YC Directory App',
-		backgroundColor: '#FFE7EB',
-		className: 'project-list-wrapper',
-	},
-	{
-		id: 'yc-directory',
-		title: 'YC Directory App for Startup Discovery',
-		image:
-			'https://images.unsplash.com/photo-1493770348161-369560ae357d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxmb29kfGVufDB8MHx8fDE3MjEwNDI2MTR8MA&ixlib=rb-4.0.3&q=80&w=1080',
-		alt: 'YC Directory App',
-		backgroundColor: '#FFE7EB',
-		className: 'project-list-wrapper',
-	},
-	{
-		id: 'yc-directory',
-		title: 'YC Directory App for Startup Discovery',
-		image:
-			'https://images.unsplash.com/photo-1493770348161-369560ae357d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxmb29kfGVufDB8MHx8fDE3MjEwNDI2MTR8MA&ixlib=rb-4.0.3&q=80&w=1080',
-		alt: 'YC Directory App',
-		backgroundColor: '#FFE7EB',
-		className: 'project-list-wrapper',
-	},
-	{
-		id: 'yc-directory',
-		title: 'YC Directory App for Startup Discovery',
-		image:
-			'https://images.unsplash.com/photo-1493770348161-369560ae357d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxmb29kfGVufDB8MHx8fDE3MjEwNDI2MTR8MA&ixlib=rb-4.0.3&q=80&w=1080',
-		alt: 'YC Directory App',
-		backgroundColor: '#FFE7EB',
-		className: 'project-list-wrapper',
-	},
-	// Add more projects easily
 ];
