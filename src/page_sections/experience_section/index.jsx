4aea1ee575a6574da495bad11b48c4aa24bc7fa5
Bilal Masters <ceriustech@gmail.com>
2025-09-25 23:57:52 -0500

    chore: reduce section margin

--- src/page_sections/experience_section/index.jsx
+++ src/page_sections/experience_section/index.jsx
@@ -6,7 +6,7 @@ const ExperienceSection = () => {
 	return (
 		<section
 			id="experience"
-			className="flex-center md:mt-40 mt-20 section-padding xl:px-0"
+			className="flex-center md:mt-10 mt-20 section-padding xl:px-0"
 		>
 			<div className="w-full h-full md:px-20 px-5">
 				<TitleHeader title="💼 My Career Overview" />
