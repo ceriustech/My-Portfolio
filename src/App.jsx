5f3995d9e8a78522debca660c02dc652752beaf4
Bilal Masters <ceriustech@gmail.com>
2025-10-04 00:02:46 -0500

    chore: test performance on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 		<>
 			<NavBar />
 			<HeroSection />
-			{/* <ExperienceSection /> */}
+			<ExperienceSection />
 			<PortfolioSection />
 			{/* <UpcomingAppsShowcase /> */}
 			{/* <Skills /> */}
